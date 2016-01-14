5169d75a7bbc89cd7c965bf9b7e99708d338c5b6
Harsh Kumar Lamba <hlamba@sapient.com>
2016-01-14 12:30:37 +0530

    Commit

--- Day1/04.PrintFile/printFile.js
+++ Day1/04.PrintFile/printFile.js
@@ -1,5 +1,14 @@
 var fs = require("fs"),
 	fileName = "Sample.txt";
 
-var fileContents = fs.readFileSync(fileName, {encoding : 'utf8'});
-console.log(fileContents);
+//Sync
+//var fileContents = fs.readFileSync(fileName, {encoding : 'utf8'});
+
+//Async
+var fileContents = fs.readFile(fileName, {encoding : 'utf8'}, AsyncMethod);
+
+
+function AsyncMethod(content){
+	//Content will be got here	
+	console.log(content);
+}
